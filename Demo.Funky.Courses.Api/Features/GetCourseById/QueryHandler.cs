0fe927ef9e792a576465827b9ace46b907d96ad9
Cheranga Hatangala <cheranga@gmail.com>
2022-07-20 21:23:18 +1000

    feat: using statements in funky style.

--- Demo.Funky.Courses.Api/Features/GetCourseById/QueryHandler.cs
+++ Demo.Funky.Courses.Api/Features/GetCourseById/QueryHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System.Data.SqlClient;
 using Dapper;
+using Demo.Funky.Courses.Api.Features.Shared;
 using Demo.Funky.Courses.Api.Infrastructure.DataAccess;
 using LanguageExt;
+using LanguageExt.Common;
 using static LanguageExt.Prelude;
 
 namespace Demo.Funky.Courses.Api.Features.GetCourseById;
@@ -17,18 +19,21 @@ public class QueryHandler : IQueryHandler<Query, CourseDataModel>
 
     private static string CommandText => @"select * from tblCourses where id=@id";
 
+    private Eff<SqlConnection> GetConnection(string connectionString) => EffMaybe<SqlConnection>(() => new SqlConnection(connectionString));
 
     public Aff<CourseDataModel> GetAsync(Query query) => GetCourseAsync(query);
 
     // is there a better way to write this method?
     private Aff<CourseDataModel> GetCourseAsync(Query query)=>
-        TryAsync(async () =>
+        use(GetConnection(_config.DatabaseConnectionString), connection => AffMaybe<CourseDataModel>(async () =>
         {
-            using (var connection = new SqlConnection(_config.DatabaseConnectionString))
+            await connection.OpenAsync();
+            var dataModel = await connection.QueryFirstOrDefaultAsync<CourseDataModel>(CommandText, query);
+            if (string.IsNullOrWhiteSpace(dataModel?.Id))
             {
-                await connection.OpenAsync();
-                var dataModel = await connection.QueryFirstOrDefaultAsync<CourseDataModel>(CommandText, query);
-                return string.IsNullOrWhiteSpace(dataModel?.Id) ? new CourseDataModel() : dataModel;
+                return Error.New(ErrorCodes.CourseNotFound, ErrorMessages.CourseNotFound);
             }
-        }).ToAff();
+
+            return dataModel;
+        }));
 }
